ab8c383330cb144cd967dcfde89a3b952ae5db7b
YassineElHosni <hosni.yassine.yh@gmail.com>
2024-04-30 21:33:58 +0100

    cleanup display

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { Button } from 'antd'
 
 import { sample_data } from '../../sample_data'
 import PersonForm from '../../components/PersonForm'
+
+import './index.css'
 
 export default function Home() {
     const [persons] = useState(sample_data.persons)
@@ -16,7 +18,15 @@ export default function Home() {
 
         const newPersonId = new Date().toISOString()
         if (values.partner) {
-            partnerships.push({ uid: new Date().toISOString(), partner1: newPersonId, partner2: values.partner })
+            let partner1 = newPersonId
+            let partner2 = values.partner
+
+            if (values.gender === 'FEMALE') {
+                partner2 = newPersonId
+                partner1 = values.partner
+            }
+
+            partnerships.push({ uid: new Date().toISOString(), partner1, partner2 })
         }
 
         const newPerson = { ...values, partner: undefined, uid: newPersonId }
@@ -28,6 +38,33 @@ export default function Home() {
         setAddPersonOpen(false)
     }
 
+    const getPersonName = useCallback(
+        personId => {
+            const found = persons.find(o => o.uid === personId)
+            if (!found) {
+                return 'undefined'
+            }
+            return `${found.firstName} ${found.lastName}`
+        },
+        [persons],
+    )
+
+    const getChildrenNames = useCallback(
+        partnershipId => {
+            console.log(partnershipId, { partnerships, persons })
+            const found = partnerships.find(o => o.uid === partnershipId)
+
+            const children = persons.filter(
+                o =>
+                    (o.father && [found.partner1, found.partner2].includes(o.father)) ||
+                    (o.mother && [found.partner1, found.partner2].includes(o.mother)),
+            )
+
+            return children.map(o => `${o.firstName} ${o.lastName}`).join(',')
+        },
+        [partnerships, persons],
+    )
+
     return (
         <div>
             <h1>FAMILY-TREE</h1>
@@ -37,8 +74,11 @@ export default function Home() {
                     <h4>People:</h4>
                     <ul>
                         {persons.map(o => (
-                            <li key={o.uid}>
-                                {o.firstName} {o.lastName}
+                            <li key={o.uid} className="family-tree-grid">
+                                <span>{o.uid} </span>
+                                <span>
+                                    {o.firstName} {o.lastName}
+                                </span>
                             </li>
                         ))}
                     </ul>
@@ -56,8 +96,11 @@ export default function Home() {
                     <h4>Partnerships:</h4>
                     <ul>
                         {partnerships.map(o => (
-                            <li key={o.uid}>
-                                {o.partner1} {o.partner2}
+                            <li key={o.uid} className="family-tree-grid">
+                                <span>{o.uid}</span>
+                                <span>{getPersonName(o.partner1)}</span>
+                                <span>{getPersonName(o.partner2)}</span>
+                                <span>{getChildrenNames(o.uid)}</span>
                             </li>
                         ))}
                     </ul>
