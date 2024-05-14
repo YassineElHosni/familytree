c49e1ded5c750f3412a32f99f0e7206ef844d30d
YassineElHosni <hosni.yassine.yh@gmail.com>
2024-05-14 22:34:35 +0100

    remove partnerships list from sample

--- src/sample_data.js
+++ src/sample_data.js
@@ -1,121 +1,92 @@
-export const sample_data = {
-    persons: [
-        {
-            uid: '0x1a519f61f8',
-            firstName: 'Abdelaziz',
-            lastName: 'EL HOSNI',
-            gender: 'MALE',
-            mother: '0x1a519f61fe',
-            partnerships: [
-                {
-                    uid: '0x1a519f61fa',
-                    partner1: '0x1a519f61f8',
-                    partner2: '0x1a519f61f9',
-                    children: [
-                        {
-                            uid: '0x1a519f61fb',
-                        },
-                        {
-                            uid: '0x1a519f61fc',
-                        },
-                        {
-                            uid: '0x1a519f61fd',
-                        },
-                    ],
-                },
-            ],
-        },
-        {
-            uid: '0x1a519f61f9',
-            firstName: 'Widad',
-            lastName: 'DOUGHAILI',
-            gender: 'FEMALE',
-            partnerships: [
-                {
-                    uid: '0x1a519f61fa',
-                    partner1: '0x1a519f61f8',
-                    partner2: '0x1a519f61f9',
-                    children: [
-                        {
-                            uid: '0x1a519f61fb',
-                        },
-                        {
-                            uid: '0x1a519f61fc',
-                        },
-                        {
-                            uid: '0x1a519f61fd',
-                        },
-                    ],
-                },
-            ],
-        },
-        {
-            uid: '0x1a519f61fb',
-            firstName: 'Yassine',
-            lastName: 'EL HOSNI',
-            gender: 'MALE',
-            father: '0x1a519f61f8',
-            mother: '0x1a519f61f9',
-        },
-        {
-            uid: '0x1a519f61fc',
-            firstName: 'Hamza',
-            lastName: 'EL HOSNI',
-            gender: 'MALE',
-            father: '0x1a519f61f8',
-            mother: '0x1a519f61f9',
-        },
-        {
-            uid: '0x1a519f61fd',
-            firstName: 'Zakariya',
-            lastName: 'EL HOSNI',
-            gender: 'MALE',
-            father: '0x1a519f61f8',
-            mother: '0x1a519f61f9',
-        },
-        {
-            uid: '0x1a519f61fe',
-            firstName: 'Lakbira',
-            lastName: 'BEN ACHIR',
-            gender: 'FEMALE',
-            partnerships: [
-                {
-                    uid: '0x1a519f61ff',
-                    partner2: '0x1a519f61fe',
-                    children: [
-                        {
-                            uid: '0x1a519f61f8',
-                        },
-                    ],
-                },
-            ],
-        },
-    ],
-    partnerships: [
-        {
-            uid: '0x1a519f61fa',
-            partner1: '0x1a519f61f8',
-            partner2: '0x1a519f61f9',
-            children: [
-                {
-                    uid: '0x1a519f61fb',
-                },
-                {
-                    uid: '0x1a519f61fc',
-                },
-                {
-                    uid: '0x1a519f61fd',
-                },
-            ],
-        },
-        {
-            uid: '0x1a519f61ff',
-            partner2: '0x1a519f61fe',
-            children: [
-                {
-                    uid: '0x1a519f61f8',
-                },
-            ],
-        },
-    ],
-}
+export const sample_data = [
+    {
+        uid: '0x1a519f61f8',
+        firstName: 'Abdelaziz',
+        lastName: 'EL HOSNI',
+        gender: 'MALE',
+        mother: '0x1a519f61fe',
+        partnerships: [
+            {
+                uid: '0x1a519f61fa',
+                partner1: '0x1a519f61f8',
+                partner2: '0x1a519f61f9',
+                children: [
+                    {
+                        uid: '0x1a519f61fb',
+                    },
+                    {
+                        uid: '0x1a519f61fc',
+                    },
+                    {
+                        uid: '0x1a519f61fd',
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        uid: '0x1a519f61f9',
+        firstName: 'Widad',
+        lastName: 'DOUGHAILI',
+        gender: 'FEMALE',
+        partnerships: [
+            {
+                uid: '0x1a519f61fa',
+                partner1: '0x1a519f61f8',
+                partner2: '0x1a519f61f9',
+                children: [
+                    {
+                        uid: '0x1a519f61fb',
+                    },
+                    {
+                        uid: '0x1a519f61fc',
+                    },
+                    {
+                        uid: '0x1a519f61fd',
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        uid: '0x1a519f61fb',
+        firstName: 'Yassine',
+        lastName: 'EL HOSNI',
+        gender: 'MALE',
+        father: '0x1a519f61f8',
+        mother: '0x1a519f61f9',
+    },
+    {
+        uid: '0x1a519f61fc',
+        firstName: 'Hamza',
+        lastName: 'EL HOSNI',
+        gender: 'MALE',
+        father: '0x1a519f61f8',
+        mother: '0x1a519f61f9',
+    },
+    {
+        uid: '0x1a519f61fd',
+        firstName: 'Zakariya',
+        lastName: 'EL HOSNI',
+        gender: 'MALE',
+        father: '0x1a519f61f8',
+        mother: '0x1a519f61f9',
+    },
+    {
+        uid: '0x1a519f61fe',
+        firstName: 'Lakbira',
+        lastName: 'BEN ACHIR',
+        gender: 'FEMALE',
+        partnerships: [
+            {
+                uid: '0x1a519f61ff',
+                partner2: '0x1a519f61fe',
+                children: [
+                    {
+                        uid: '0x1a519f61f8',
+                    },
+                ],
+            },
+        ],
+    },
+]
