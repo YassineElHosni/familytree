9f3d2fed26d09206213e026b9aad8d2936ff1b90
YassineElHosni <hosni.yassine.yh@gmail.com>
2024-05-14 21:03:27 +0100

    update simple data to match main get

--- src/sample_data.js
+++ src/sample_data.js
@@ -1,55 +1,114 @@
 export const sample_data = {
     persons: [
-    {
-      uid: '0x1a519f37fe',
-      firstName: 'Abdelaziz',
-      lastName: 'EL HOSNI',
-      gender: 'MALE',
-      mother: '0x1a519f3804'
-    },
-    {
-      uid: '0x1a519f37ff',
-      firstName: 'Widad',
-      lastName: 'DOUGHAILI',
-      gender: 'FEMALE'
-    },
-    {
-      uid: '0x1a519f3801',
-      firstName: 'Yassine',
-      lastName: 'EL HOSNI',
-      gender: 'MALE',
-      father: '0x1a519f37fe',
-      mother: '0x1a519f37ff'
-    },
-    {
-      uid: '0x1a519f3802',
-      firstName: 'Hamza',
-      lastName: 'EL HOSNI',
-      gender: 'MALE',
-      father: '0x1a519f37fe',
-      mother: '0x1a519f37ff'
-    },
-    {
-      uid: '0x1a519f3803',
-      firstName: 'Zakariya',
-      lastName: 'EL HOSNI',
-      gender: 'MALE',
-      father: '0x1a519f37fe',
-      mother: '0x1a519f37ff'
-    },
-    {
-      uid: '0x1a519f3804',
-      firstName: 'Lakbira',
-      lastName: 'BEN ACHIR',
-      gender: 'FEMALE'
-    }
-  ],
-  partnerships: [
-    {
-      uid: '0x1a519f3800',
-      partner1: '0x1a519f37fe',
-      partner2: '0x1a519f37ff'
-    },
-    { uid: '0x1a519f3805', partner2: '0x1a519f3804' }
-  ]
-  }
+        {
+            uid: '0x1a519f61f8',
+            firstName: 'Abdelaziz',
+            lastName: 'EL HOSNI',
+            gender: 'MALE',
+            partnerships: [
+                {
+                    uid: '0x1a519f61fa',
+                    partner1: '0x1a519f61f8',
+                    partner2: '0x1a519f61f9',
+                    children: [
+                        {
+                            uid: '0x1a519f61fb',
+                        },
+                        {
+                            uid: '0x1a519f61fc',
+                        },
+                        {
+                            uid: '0x1a519f61fd',
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            uid: '0x1a519f61f9',
+            firstName: 'Widad',
+            lastName: 'DOUGHAILI',
+            gender: 'FEMALE',
+            partnerships: [
+                {
+                    uid: '0x1a519f61fa',
+                    partner1: '0x1a519f61f8',
+                    partner2: '0x1a519f61f9',
+                    children: [
+                        {
+                            uid: '0x1a519f61fb',
+                        },
+                        {
+                            uid: '0x1a519f61fc',
+                        },
+                        {
+                            uid: '0x1a519f61fd',
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            uid: '0x1a519f61fb',
+            firstName: 'Yassine',
+            lastName: 'EL HOSNI',
+            gender: 'MALE',
+        },
+        {
+            uid: '0x1a519f61fc',
+            firstName: 'Hamza',
+            lastName: 'EL HOSNI',
+            gender: 'MALE',
+        },
+        {
+            uid: '0x1a519f61fd',
+            firstName: 'Zakariya',
+            lastName: 'EL HOSNI',
+            gender: 'MALE',
+        },
+        {
+            uid: '0x1a519f61fe',
+            firstName: 'Lakbira',
+            lastName: 'BEN ACHIR',
+            gender: 'FEMALE',
+            partnerships: [
+                {
+                    uid: '0x1a519f61ff',
+                    partner2: '0x1a519f61fe',
+                    children: [
+                        {
+                            uid: '0x1a519f61f8',
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
+    partnerships: [
+        {
+            uid: '0x1a519f61fa',
+            partner1: '0x1a519f61f8',
+            partner2: '0x1a519f61f9',
+            children: [
+                {
+                    uid: '0x1a519f61fb',
+                },
+                {
+                    uid: '0x1a519f61fc',
+                },
+                {
+                    uid: '0x1a519f61fd',
+                },
+            ],
+        },
+        {
+            uid: '0x1a519f61ff',
+            partner2: '0x1a519f61fe',
+            children: [
+                {
+                    uid: '0x1a519f61f8',
+                },
+            ],
+        },
+    ],
+}
